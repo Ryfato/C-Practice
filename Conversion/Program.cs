e74d7fd61ebb38bf961f06a2c6a534eed96a4cb9
Ryfato Rono <ryfato2021@gmail.com>
2020-07-12 22:54:17 +0700

    Add C sharp: Temperature Project

---
+++ Conversion/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Conversion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Temperture Conversion");
+            double celcius;
+            double fahrenhit;
+            double kelvin;
+            for(int i=1; i<=20; i++)
+            {
+                celcius = 25 * i;
+                Console.WriteLine( celcius+"C = "+Temperature.celciusToKelvin(celcius)+"K");
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Conversion/Temperature.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Conversion
+{
+    static class Temperature
+    {
+        //Convert Celcius to Fahrenhit
+        public static double celciusToFahrenhit(double celcius)
+        {
+            return (celcius * 9 / 5) + 32;
+        }
+        // Convert Fahrenhit to Celcius
+        public static double fahrenhitToCelcius(double fahrenhit)
+        {
+            return (fahrenhit- 32) *5 / 9;
+        }
+        public static double celciusToKelvin(double celcius)
+        {
+            return celcius + 273.15;
+        }
+        public static double kelvinToCelcius(double kelvin)
+        {
+            return kelvin - 273.15;
+        }
+    }
+}
